17b09847a0692c8f8796c9d96e838e1091c67918
ayin1119 <603265020@qq.com>
2026-06-15 16:29:36 +0800

    Add files via upload

---
+++ 表/用户/用户留存与复购/留存与复购/整体复购率.sql
@@ -0,0 +1,11 @@
+SELECT
+    COUNT(*) AS total_users,
+    SUM(CASE WHEN purchase_count >= 2 THEN 1 ELSE 0 END) AS repurchase_users,
+    ROUND(SUM(CASE WHEN purchase_count >= 2 THEN 1 ELSE 0 END) / COUNT(*), 4) AS repurchase_rate
+FROM (
+    SELECT
+        user_id,
+        COUNT(CASE WHEN behavior_type = 4 THEN 1 END) AS purchase_count
+    FROM data_min
+    GROUP BY user_id
+) t;
